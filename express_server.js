6fa4ac9e866b42fa513d7ca3b4f3232ed8e9fd99
Jeff Bush <jeffbushdesign@gmail.com>
2022-03-06 22:06:10 +0000

    Display username when logged in

--- express_server.js
+++ express_server.js
@@ -44,8 +44,12 @@ app.listen(PORT, () => {
 // add additional endpoints
 // when you're entering a new url
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"],
+  };
+  res.render("urls_new", templateVars);
 });
+
 
 
 app.get("/urls.json", (req, res) => {
@@ -56,13 +60,26 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+
+
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"],
+  };
   res.render("urls_index", templateVars);
 });
 
+
+
+
+
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = {
+    username: req.cookies["username"],
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL]
+  };
   res.render("urls_show", templateVars);
 });
 
@@ -91,6 +108,11 @@ app.post("/login", (req, res) => {
   res.cookie('username', username);
 
   // After our server has set the cookie it should redirect the browser back to the /urls page.
+  res.redirect('/urls');
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie('username');
   res.redirect('/urls');
 });
 
